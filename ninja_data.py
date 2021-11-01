a58b3c65f7a550ea0642e870fa5ee0e2d7c5dff6
xanthics <3137623+xanthics@users.noreply.github.com>
2021-10-31 22:30:51 -0700

    market update, bugfix scourge drop restricted mods were not properly blocked

--- ninja_data.py
+++ ninja_data.py
@@ -117,6 +117,8 @@ def scrape_ninja():
 		"Blackflame", "Qotra's Regulator", "Rotblood Promise", "Survivor's Guilt", "Hands of the Fervent",
 		# Ultimatum
 		"Atziri's Rule", "Cane of Kulemak", "Glimpse of Chaos", "Hateforge", "Mahuxotl's Machination", "Relic of the Pact", "Steelworm", "Temptation Step", "The Scales of Justice", "Triumvirate Authority", "Yaomac's Accord",
+		# Scourge
+		"Strangegrasp", "Uul-Netol's Vow", "Stasis Prison",
 	}
 
 	keys = [
